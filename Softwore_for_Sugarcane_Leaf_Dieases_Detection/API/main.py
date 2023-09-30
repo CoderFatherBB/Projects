ca7c2ed7e0ac71e1ecc082e71f55ce61100c4a14
Bhavin Baldota <baldotabhavin391@gmail.com>
2023-09-30 13:39:47 +0530

    Update

---
+++ Softwore_for_Sugarcane_Leaf_Dieases_Detection/API/main.py
@@ -0,0 +1,54 @@
+from fastapi import FastAPI, File, UploadFile
+from fastapi.middleware.cors import CORSMiddleware
+import uvicorn
+import numpy as np
+from io import BytesIO
+from PIL import Image
+import tensorflow as tf
+
+app = FastAPI()
+
+# origins = [
+#     "http://localhost",
+#     "http://localhost:3000",
+# ]
+
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=origins,
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
+
+MODEL = tf.keras.models.load_model("../Models/Version_1")
+
+CLASS_NAMES = ['Banded_Chlorosis', 'Brown_Rust', 'Brown_Spot', 'Dried_Leaves', 'Grassy_Shoot', 'Healthy_Leaves', 'Pokkah_Boeng', 'Sett_Rot', 'Smut', 'Viral_Disease', 'Yellow_Leaf']
+
+@app.get("/ping")
+async def ping():
+    return "Hello, I am alive"
+
+def read_file_as_image(data) -> np.ndarray:
+    image = np.array(Image.open(BytesIO(data)))
+    return image
+
+@app.post("/predict")
+async def predict(
+    file: UploadFile = File(...)
+):
+    image = read_file_as_image(await file.read())
+    img_batch = np.expand_dims(image, 0)
+    
+    predictions = MODEL.predict(img_batch)
+
+    predicted_class = CLASS_NAMES[np.argmax(predictions[0])]
+    confidence = np.max(predictions[0])
+
+    return {
+        'class': predicted_class,
+        'confidence': float(confidence)
+    }
+
+if __name__ == "__main__":
+    uvicorn.run(app, host='localhost', port=8000)
